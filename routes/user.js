9dcac1d897af150128b54058cef79b13619ec380
Bilal raza <attari1235@gmail.com>
2023-10-10 06:11:52 -0700

    Delete Update Routes added

--- routes/user.js
+++ routes/user.js
@@ -39,9 +39,27 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     console.log(req.body)
-    users.push({name :req.body.name , id : users.length + 1 })
-    res.status(200).send({ status: 200, user : {name :req.body.name , id : users.length + 1 } })
+    users.push({ name: req.body.name, id: users.length + 1 })
+    res.status(200).send({ status: 200, user: { name: req.body.name, id: users.length + 1 } })
+})
 
+
+router.delete('/:id', (req, res) => {
+    users.splice(req.params.id - 1, 1)
+    res.status(200).send({ status: 200, users })
+})
+
+router.put('/:id', (req, res) => {
+
+    if (users[req.params.id - 1]) {
+        users[req.params.id - 1].name = req.body.name
+        res.status(200).send({ status: 200,  user : users[req.params.id - 1] })
+
+    } else {
+        
+
+    }
+    res.status(200).send({ status: 200, users })
 })
 
 
